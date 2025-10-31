fd664e65c0c9b7369277e91072601e6eef9a92fc
ajbeck800 <ajbeck800@gmail.com>
2025-10-31 16:41:11 +0000

    Updated Customer.java

--- Customer.java
+++ Customer.java
@@ -22,39 +22,35 @@ public class Customer extends User{
 	} // end constructor
 
 	public void start(){
-		System.out.println("Welcome, " + userName + "!");
+		System.out.println("Login Successful");
 		boolean keepGoing = true;
 		while (keepGoing == true){
-			switch (menu()){
-				case "1":
-					checking.start();
-					break;
-				case "2":
-					savings.start();
-					break;
-				case "3":
-					changePin();
-					break;
-				case "4":
-					System.out.println(getReport());
-					break;
-				case "5":
-					keepGoing = false;
-					System.out.println("Goodbye, " + userName + "!");
-					break;
-				default: System.out.println("Invalid option.");
-			} // end switch
+			String menuRequest = c.menu();
+			if (menuRequest.equals("0")){
+				keepGoing = false;
+			} // end if
+			else if (menuRequest.equals("1")){
+				checking.start();
+			} // end else if
+			else if (menuRequest.equals("2")){
+				savings.start();
+			} // end else if
+			else if (menuRequest.equals("3")){
+                                changePin();
+                        } // end else if
+			else {
+				System.out.println("Invalid selection.");
+			} // end else
 		} // end while
 	} // end start
 
 	public String menu(){
-		System.out.println("\n=== Customer Menu ===");
-		System.out.println("1. Checking Account");
-		System.out.println("2. Savings Account");
-		System.out.println("3. Change PIN");
-		System.out.println("4. View Report");
-		System.out.println("5. Exit");
-		System.out.print("Enter your choice: ");
+		System.out.println("Customer Menu");
+		System.out.println("0) Exit");
+		System.out.println("1) Manage Checking Account");
+		System.out.println("2) Manage Savings Account");
+		System.out.println("3) Change PIN");
+		System.out.print("Enter 0-3: ");
 		return input.nextLine();
 	} // end menu
 
@@ -66,6 +62,6 @@ public class Customer extends User{
 	} // end changePIN
 	
 	public String getReport(){
-		return "User Report:\nUsername: " + userName + "\nPIN: " + PIN + "\nChecking Balance: " + checking.getBalanceString() + "\nSavings Balance: " + savings.getBalanceString();
+
 	} // end getReport
 } // end
