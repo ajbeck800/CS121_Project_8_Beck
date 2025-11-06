e12e9199ce9dbbe1b1a9bd45df93287bb9d8333b
ajbeck800 <ajbeck800@gmail.com>
2025-11-06 23:51:04 +0000

    Updated Bank.java

--- Bank.java
+++ Bank.java
@@ -47,6 +47,7 @@ public class Bank implements HasMenu{
 	
 	public void applyInterest(){
 		for (Customer c : customers) {
+			c.savings.setInterestRate();
 			c.savings.calcInterest();
 		} // end for
 	} // end applyInterest
